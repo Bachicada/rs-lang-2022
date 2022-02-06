6ace4595f8580af986ad618cf372c7622886a0f5
Bachicada <bachicada@gmail.com>
2022-02-06 22:01:31 +0300

    Updates

--- my-app/lang-files/src/types.tsx
+++
@@ -1,9 +0,0 @@
-export enum Styling {
-   headerHeight = '40'
-}
-
-declare module 'react' {
-    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-      custom?: string;
-    }
-  }
