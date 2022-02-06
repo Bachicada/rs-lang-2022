8319aa050fb8c10f60834c5337171b59483025b9
Bachicada <bachicada@gmail.com>
2022-02-06 18:48:39 +0300

    feat:start-page

---
+++ src/components/start-page/Landing.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import styles from './landing.module.css';
+import {AboutApp} from './AboutApp';
+import {Benefits} from './Benefits';
+import {Team} from './Team';
+
+import { Styling } from '../../types';
+
+export class Landing extends React.Component {
+    render(){
+     return (
+       <div className={styles.landing} >
+          <AboutApp />
+          <Benefits />
+          <Team />
+       </div>
+     )
+    }
+}
