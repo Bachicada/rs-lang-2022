e20de1597affe3367da00571020e5251296b463b
timursk <timur.dev@mail.ru>
2022-02-08 20:35:21 +0300

    feat: change popup to modal, add containers

--- my-app/src/components/sprint/LevelModal.tsx
+++ my-app/src/components/sprint/LevelModal.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import React from 'react';
 
 interface ModalProps {
@@ -13,9 +13,17 @@ const LevelModal = () => {
   const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      
-    </div>
+    <Container maxWidth="sm">
+      <h2 style={{textAlign: 'center'}}>Выбери свой уровень</h2>
+      <Container maxWidth="sm" sx={{ display: 'flex', justifyContent: 'space-between', }}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </Container>
+    </Container>
   );
 };
 
--- my-app/src/components/sprint/Sprint.tsx
+++ my-app/src/components/sprint/Sprint.tsx
@@ -8,11 +8,16 @@ interface SprintProps {
 }
 
 const Sprint: FC<SprintProps> = (props) => {
+  const [level, setLevel] = React.useState(props.level || 0);
+  
+
   return (
     <Box
       sx={{
         width: '100%',
         height: 'calc(100vh - 64px - 25px - 48px)',
+        display: 'flex',
+        alignItems: 'center',
       }}
     >
       <LevelModal></LevelModal>
