596ea2b7e14c9bb44781f7ae88d9f8b886d7d4e3
unknown <timur.dev@mail.ru>
2022-02-18 01:30:53 +0300

    feat: add starsCounter

--- my-app/src/components/sprint/SprintGame.tsx
+++ my-app/src/components/sprint/SprintGame.tsx
@@ -1,10 +1,11 @@
 import { Container } from '@mui/material'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react'
 import { createUserWord, updateUserWord } from '../../services/UserWordService'
 import { WordItem } from '../../types'
 import { API_URL, WORD_STATUS } from '../../utils/Constants'
 import { LoadingIcon } from '../shared/LoadingIcon'
 import { GameAnswers, IWords } from './Sprint'
+import SprintStars from './SprintStars'
 
 interface SprintGameProps {
   words: IWords[];
@@ -24,6 +25,8 @@ interface Answer {
 }
 
 const SprintGame = (props: SprintGameProps) => {
+  const [starsCount, setStarsCount] = useState<number>(1);
+
   if (!props.isGameReady) {
     return (
     <div style={{position: 'absolute', zIndex: '50', width: 'auto', height: 'auto', 
@@ -38,11 +41,10 @@ const SprintGame = (props: SprintGameProps) => {
   const obj = props.words[props.wordsId];
   const item = obj.item;
   const audio = new Audio(`${API_URL}/${item.audio}`);
-
   return (
     <Container maxWidth="md" style={{ background: 'rgb(153, 207, 51)', borderRadius: '5px', display: 'flex', 
         alignItems: 'center', flexDirection: 'column' }}>
-      <div>* * *</div>
+      <SprintStars count={starsCount} />
       <button onClick={() => {
         audio.play();
       }}>Play word</button>
@@ -60,16 +62,21 @@ const SprintGame = (props: SprintGameProps) => {
             answer.successCounter = item.optional.successCounter;
           });
           props.gameAnswers.push(answer);
+          setStarsCount((starsCount) => obj.correct ? 1 : (starsCount + 1));
           props.setWordsId(props.wordsId + 1);
         }}>Неверно</button>
         <button onClick={() => {
-          props.gameAnswers.push(
-            obj.correct ? {item: item, answer: true} : {item: item, answer: false}
-          );
-          updateUserWord({ wordId: `${item.id}`, word: { difficulty: WORD_STATUS.NEW, optional: { 
+          const answer: Answer = obj.correct ? {item: item, answer: true} : {item: item, answer: false}; 
+          const content = updateUserWord({ wordId: `${item.id}`, word: { difficulty: WORD_STATUS.NEW, optional: { 
             failCounter: obj.correct ? 0 : 1,
             successCounter: obj.correct ? 1 : 0
-           }}})
+           }}});
+          content.then((item) => {
+            answer.failCounter = item.optional.failCounter; 
+            answer.successCounter = item.optional.successCounter;
+          });
+          props.gameAnswers.push(answer);
+          setStarsCount((starsCount) => obj.correct ? (starsCount + 1) : 1);
           props.setWordsId(props.wordsId + 1);
         }}>Верно</button>
       </div>
---
+++ my-app/src/components/sprint/SprintStars.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface SprintStarsProps {
+  count: number;
+}
+
+const SprintStars = (props: SprintStarsProps) => {
+  const starsArr = [];
+  for (let i = 0; i < props.count; i++) {
+    starsArr.push('*');
+  }
+  return (
+    <p>
+      {starsArr}
+    </p>
+  )
+}
+
+export default SprintStars
