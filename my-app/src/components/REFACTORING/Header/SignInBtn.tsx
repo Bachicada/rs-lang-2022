6074e727a01104bc026d239254450bbc3d7db445
unknown <timur.dev@mail.ru>
2022-06-20 00:43:28 +0300

    refactor: rewrite AboutApp component, fix signin btn color

--- my-app/src/components/REFACTORING/Header/SignInBtn.tsx
+++ my-app/src/components/REFACTORING/Header/SignInBtn.tsx
@@ -1,12 +1,16 @@
-import { Button } from '@mui/material';
+import { Button, styled } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { APP_ROUTES } from '../../../utils/Constants';
 
+const StyledLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.secondary.main,
+}));
+
 const SignInBtn = () => {
   return (
     <Button color="secondary" variant="outlined">
-      <Link to={APP_ROUTES.SIGNIN}>Войти</Link>
+      <StyledLink to={APP_ROUTES.SIGNIN}>Войти</StyledLink>
     </Button>
   );
 };
--- my-app/src/components/start-page/AboutApp.tsx
+++ my-app/src/components/start-page/AboutApp.tsx
@@ -1,22 +1,64 @@
 import * as React from 'react';
 import styles from './landing.module.css';
-import topBanner from '../../assets/creative_minds.png';
-import logoImg from '../../assets/rs-logo1.svg';
+// import topBanner from '../../assets/creative_minds.png';
+import topBanner from '../../assets/gifs/student.png';
+// import logoImg from '../../assets/rs-logo1.svg';
+import logoImg from '../../assets/rslang-logo.png';
+import { styled, Typography } from '@mui/material';
+// import styled from '@emotion/styled';
+
+const StyledSection = styled('section')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+`;
+
+const ImgContainer = styled('div')`
+  flex: 1 1 50%;
+`;
+
+const StyledImg = styled('img')`
+  width: 100%;
+`;
+
+const TextContainer = styled('div')`
+  flex: 1 1 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: right;
+`;
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '3rem',
+  },
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '3.75rem',
+  },
+  [theme.breakpoints.up('lg')]: {
+    fontSize: '6rem',
+  },
+}));
 
 export class AboutApp extends React.Component {
   render() {
     return (
-      <section className={styles.section + ' ' + styles.aboutSection}>
-        <div className={styles.imgContainer}>
-          <img className={styles.innerImg} src={topBanner} alt="app top banner" />
-        </div>
-        <div className={styles.logoCont}>
-          <img className={styles.rsLogo} src={logoImg} alt="app-logo" />
-          <h2 className={styles.mainTitle}>
-            Приложение для изучения английского языка для пользователей разного уровня знаний
-          </h2>
-        </div>
-      </section>
+      <StyledSection>
+        <ImgContainer>
+          <StyledImg src={topBanner} alt="student logo" />
+        </ImgContainer>
+        <TextContainer>
+          <StyledTypography variant="h1">
+            Твой помощник в изучении английского языка
+          </StyledTypography>
+        </TextContainer>
+      </StyledSection>
     );
   }
 }
