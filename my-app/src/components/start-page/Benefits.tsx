d2b8efd6f9f2390bc12aca8b51324c37cf76c80f
unknown <timur.dev@mail.ru>
2022-06-23 03:27:50 +0300

    refactor: remove unnecessary & commented code, fix imgs names & imports

--- my-app/src/components/start-page/Benefits.tsx
+++ my-app/src/components/start-page/Benefits.tsx
@@ -1,11 +1,5 @@
 import * as React from 'react';
-import styles from './landing.module.css';
-import textbookIcon from '../../assets/textbookIcon1.svg';
-import sprintIcon from '../../assets/sprintIcon.svg';
-import audioIcon from '../../assets/audioIcon.svg';
-import statIcon from '../../assets/statIcon1.svg';
-import { APP_ROUTES } from '../../utils/Constants';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Stack, styled, Typography } from '@mui/material';
 import bookImg from '../../assets/benefits/book.png';
 import joystickImg from '../../assets/benefits/joystick.png';
@@ -45,6 +39,13 @@ const StyledStack = styled(Stack)`
   }
 `;
 
+const stackContent = [
+  { img: bookImg, title: 'Учебник', subtitle: 'Содержит более 3000 слов', link: '/textbook/0/1' },
+  { img: controllerImg, title: 'Спринт', subtitle: 'Угадывай слова на время', link: '/sprint' },
+  { img: joystickImg, title: 'Аудиовызов', subtitle: 'Слово по звучанию', link: '/audiocall' },
+  { img: chartImg, title: 'Статистика', subtitle: 'Прогресс изучения', link: '/statistics' },
+];
+
 export function Benefits() {
   const navigate = useNavigate();
 
@@ -53,6 +54,7 @@ export function Benefits() {
       <Typography sx={{ mb: 3 }} variant="h4">
         Давай начнем учиться
       </Typography>
+
       <StyledStack
         sx={{
           justifyContent: 'space-between',
@@ -61,33 +63,16 @@ export function Benefits() {
         }}
         direction={{ xs: 'column', sm: 'row' }}
         spacing={1}
-        // direction={{ xs: 'column', sm: 'row' }}
-        // spacing={{ xs: 2, sm: 2, md: 4 }}
       >
-        <StyledItem>
-          <img src={bookImg} alt="controller" />
-          <Typography variant="h6">Учебник</Typography>
-          <Typography variant="body1">Содержит более 3000 слов</Typography>
-        </StyledItem>
-        <StyledItem
-          onClick={() => {
-            navigate('/sprint');
-          }}
-        >
-          <img src={controllerImg} alt="controller" />
-          <Typography variant="h6">Спринт</Typography>
-          <Typography variant="body1">Угадывай слова на время</Typography>
-        </StyledItem>
-        <StyledItem>
-          <img src={joystickImg} alt="controller" />
-          <Typography variant="h6">Аудиовызов</Typography>
-          <Typography variant="body1">Угадай слово по звучанию</Typography>
-        </StyledItem>
-        <StyledItem>
-          <img src={chartImg} alt="controller" />
-          <Typography variant="h6">Статистика</Typography>
-          <Typography variant="body1">Прогресс изучения</Typography>
-        </StyledItem>
+        {stackContent.map(({ img, title, subtitle, link }, idx) => (
+          <StyledItem key={idx} onClick={() => navigate(link)}>
+            <img src={img} alt="controller" />
+
+            <Typography variant="h6">{title}</Typography>
+
+            <Typography variant="body1">{subtitle}</Typography>
+          </StyledItem>
+        ))}
       </StyledStack>
     </section>
   );
--- my-app/src/components/start-page/Team.tsx
+++ my-app/src/components/start-page/Team.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import styles from './landing.module.css';
-// import maryPic from '../../assets/maryPic.svg';
-// import timPic from '../../assets/timPic.svg';
-import gitIcon from '../../assets/git-icon.png';
 import { Stack, styled, Typography } from '@mui/material';
 import maryImg from '../../assets/illustrations/womanLaptopGraph.png';
-import timImg from '../../assets/illustrations/manLaptop.png';
+import timImg from '../../assets/illustrations/manLaptopCropped.png';
 
 const StyledWrapper = styled('div')`
   position: relative;
@@ -13,7 +9,6 @@ const StyledWrapper = styled('div')`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  // width: 45%;
   width: 100%;
   max-width: 650px;
   height: 330px;
@@ -24,93 +19,51 @@ const StyledWrapper = styled('div')`
 `;
 
 const StyledImg = styled('img')`
-  // position: absolute;
   width: 100%;
   max-width: 300px;
-  // transform: translateY(28%);
-  // top: -20%;
 `;
 
-export class Team extends React.Component {
-  render() {
-    return (
-      <section>
-        <Typography sx={{ marginBottom: '40px' }} variant="h4">
-          Команда разработчиков
-        </Typography>
-        <Stack
-          // direction="row"
-          direction={{ xs: 'column', sm: 'row' }}
-          spacing={2}
-          sx={{ alignItems: 'center', justifyContent: 'space-around' }}
-        >
-          <StyledWrapper>
-            <StyledImg src={timImg} alt="timur" />
-            <Typography variant="h6">Тимур Салихов</Typography>
-            <Typography>Teamlead, frontend developer</Typography>
+const stackContent = [
+  {
+    img: timImg,
+    name: 'Тимур Салихов',
+    role: 'Teamlead, frontend developer',
+    work: 'Разработал мини-игры спринт и аудиовызов, компоненты статистики, дизайн приложения',
+  },
+  {
+    img: maryImg,
+    name: 'Мария Губа',
+    role: 'Frontend developer',
+    work: 'Разработала страницы авторизации, компоненты учебника',
+  },
+];
+
+export const Team = () => {
+  return (
+    <section>
+      <Typography sx={{ marginBottom: '40px' }} variant="h4">
+        Команда разработчиков
+      </Typography>
+
+      <Stack
+        direction={{ xs: 'column', sm: 'row' }}
+        spacing={2}
+        sx={{ alignItems: 'center', justifyContent: 'space-around' }}
+      >
+        {stackContent.map(({ img, name, role, work }, idx) => (
+          <StyledWrapper key={idx}>
+            <StyledImg src={img} alt={name} />
+
+            <Typography variant="h6">{name}</Typography>
+
+            <Typography>{role}</Typography>
+
             <Typography sx={{ fontFamily: 'Roboto !important', padding: '0 5%' }}>
-              Разработал мини-игры спринт и аудиовызов, компоненты статистики, дизайн приложения
+              {work}
             </Typography>
           </StyledWrapper>
-          <StyledWrapper>
-            <StyledImg src={maryImg} alt="timur" />
-            <Typography variant="h6">Мария Губа</Typography>
-            <Typography>Frontend developer</Typography>
-            <Typography sx={{ fontFamily: 'Roboto !important', padding: '0 5%' }}>
-              Разработала страницы авторизации, компоненты учебника
-            </Typography>
-          </StyledWrapper>
-        </Stack>
-      </section>
-      // <section className="section aboutSection">
-      //   <h2 className={styles.h2}>Команда разработчиков</h2>
-      //   <div className={styles.profCardsCont}>
-      //     <div className={styles.profCard}>
-      // {/* <img className={styles.profPic} src={maryPic} alt="developer avatar" /> */}
-      //       <p className={styles.devName}>Мария Губа</p>
-      //       <p className={styles.devStatus}>frontend developer</p>
-      //       <p>Разработала:</p>
-      //       <ul>
-      //         <li>Главную страницу приложения</li>
-      //         <li>Страницы Авторизации</li>
-      //         <li>Компоненты Учебника</li>
-      //       </ul>
-      //       <div className={styles.gitLink}>
-      //         <a
-      //           className="footerLink"
-      //           href="https://github.com/Bachicada"
-      //           target="_blank"
-      //           rel="noreferrer"
-      //         >
-      //           <span>bachicada</span>
-      //           <img className={styles.gitIcon} src={gitIcon} />
-      //         </a>
-      //       </div>
-      //     </div>
-      //     <div className={styles.profCard}>
-      //       <img className={styles.profPic} src={timPic} alt="developer avatar" />
-      //       <p className={styles.devName}>Тимур Салихов</p>
-      //       <p className={styles.devStatus}>Team leader, frontend developer</p>
-      //       <p>Разработал:</p>
-      //       <ul>
-      //         <li>Мини-игру Спринт</li>
-      //         <li>Мини-игру Аудиовызов</li>
-      //         <li>Компоненты и логику Статистики</li>
-      //       </ul>
-      //       <div className={styles.gitLink}>
-      //         <a
-      //           className="footerLink"
-      //           href="https://github.com/timursk"
-      //           target="_blank"
-      //           rel="noreferrer"
-      //         >
-      //           <span>timursk</span>
-      //           <img className={styles.gitIcon} src={gitIcon} />
-      //         </a>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </section>
-    );
-  }
-}
+        ))}
+      </Stack>
+    </section>
+  );
+};
