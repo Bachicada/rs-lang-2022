9f08a4f5b27acb0a371656bbc344796f0f84737a
unknown <timur.dev@mail.ru>
2022-07-12 03:32:57 +0300

    refactor: upd container styles in games

--- my-app/src/components/AudiocallGame/styles.ts
+++ my-app/src/components/AudiocallGame/styles.ts
@@ -16,4 +16,12 @@ export const StyledContainer = styled(Container)`
   justify-content: space-between;
   min-height: 400px;
   border-radius: 5px;
+
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px rgb(0 0 0 / 14%);
+  background-color: #ffffffd6;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  padding: 20px;
+  min-height: 450px;
 `;
